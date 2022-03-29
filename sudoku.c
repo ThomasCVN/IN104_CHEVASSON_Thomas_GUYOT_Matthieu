ad061711252dd7b2323af611bca1e66cb1ea15a3
Thomas <thomas.chevasson@ensta-paris.fr>
2022-03-29 16:09:59 +0200

    test

---
+++ sudoku.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+
+#define n (9)
+
+int m = sqrt(n);
+
+	
+
+void remplir_diago (int **grille)
+{
+	for (int i = 1; i<m; i+=m+1)
+	{
+		int j = 1;
+		while (j<=9)
+		{
+			srand(time(NULL));
+			int k = rand()%m+1;
+			printf("%d",grille[i][k]);
+			if (grille[i][k]==0)
+			{
+				grille[i][k] = j;
+				j = j+1;
+			}
+		}
+	}
+}
+
+
+void print_grille (int **grille)
+{
+	int m = sqrt(n);
+	for (int i=1; i<m; i++)
+	{
+		for (int j=1; j<m; j++)
+		{
+			printf("%d ",grille[i][j]);
+		}
+		printf("\n");
+	}
+}
+	
+			
+
+
+int main()
+{
+	int grille[n][n] = calloc(sizeof(int)*n);
+	remplir_diago(grille);
+	print_grille(grille,n);
+}
+
+
+
+
+
+
+
+
+
