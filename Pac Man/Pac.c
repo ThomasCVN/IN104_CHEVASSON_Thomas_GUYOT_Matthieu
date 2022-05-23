dd7b1ae7998be9826817b959e95e5836a47af9d5
MattGYT <matthieu.guyot@ensta-paris.fr>
2022-05-23 10:22:43 +0200

    ajout second fichier

--- Pac Man/Pac.c
+++ Pac Man/Pac.c
@@ -31,7 +31,7 @@ struct Ghost
 	int ny;
 };
 
-struct Ghost** allGhosts[NR_GHOSTS];
+struct Ghost* allGhosts[NR_GHOSTS];
 
 struct coordinates depart_PacMan={1,1};
 struct PacMan Pm={&depart_PacMan,0,0,3,0};
@@ -69,7 +69,7 @@ char area[H][W]={
    { "############################################################" }
 };
 
-void initialize (struct Ghost** allGhosts, char** area)
+void initialize (struct Ghost** allGhosts, char area[H][W])
 {
    srand(time(NULL));
    for (int i=0;i<NR_GHOSTS;i+=1)
@@ -77,7 +77,7 @@ void initialize (struct Ghost** allGhosts, char** area)
       struct Ghost* G=malloc (sizeof (struct Ghost));
       int x0=rand()%(H+1);
       int y0=rand()%(W+1);
-      while(y0==1 && x0==1)
+      while((y0==1 && x0==1) || area[x0][y0]=='#')
       {
          x0=rand()%(H+1);
          y0=rand()%(W+1);
@@ -93,11 +93,11 @@ void initialize (struct Ghost** allGhosts, char** area)
    }
 }
 
-void move_Pac_Man(struct PacMan* PacMan, char** area)
+void move_Pac_Man(struct PacMan* PacMan, char area[H][W])
 {
    struct coordinates* c=PacMan->coordonnees;
 
-   if (area[c->x+PacMan->nx][c->y+PacMan->ny]!='#')
+   if (area[c->x+PacMan->nx][c->y+PacMan->ny]!='#' && area[c->x+PacMan->nx][c->y+PacMan->ny]!='G')
    {
       area[c->x][c->y]=' ';
       c->x+=PacMan->nx;
@@ -111,12 +111,53 @@ void move_Pac_Man(struct PacMan* PacMan, char** area)
    else
    {
       PacMan->lives-=1;
+
       area[c->x][c->y]=' ';
       c->x=1;
       c->y=1;
       area[c->x][c->y]='P';
    }
 }
+
+void move_Ghosts(struct Ghost** allGhosts, char area[H][W])
+{
+   for (int k=0;k<NR_GHOSTS;k+=1)
+   { 
+      struct coordinates* c=allGhosts[k]->coordonnees;
+      int nx=allGhosts[k]->nx;
+      int ny=allGhosts[k]->ny;
+      if (area[c->x+nx][c->y+ny]!='#')
+      {
+         c->x=c->x+nx;
+         c->y+=ny;
+         if (area[c->x][c->y]=='.')
+         {
+            area[c->x-nx][c->y-ny]='.';
+         }
+         if (area[c->x][c->y]==' ')
+         {
+            area[c->x-nx][c->y-ny]=' ';
+
+         }
+         area[c->x][c->y]='G';
+      }
+      else
+      {
+         c->x-=nx;
+         c->y-=ny;
+         if (area[c->x][c->y]=='.')
+         {
+            area[c->x+nx][c->y+ny]='.';
+         }
+         if (area[c->x][c->y]==' ')
+         {
+            area[c->x-nx][c->y-ny]=' ';
+         }
+         area[c->x][c->y]='G';
+      }
+   }
+}
+
 
 void print_area(char area[H][W])
 {
@@ -136,23 +177,23 @@ void clavier(struct PacMan* P, struct Ghost** allGhosts)
    scanf(" %c",&direction);
    if (direction == 'z')
    {
-      P->ny = 1;
-      P->nx = 0;
+      P->nx =-1;
+      P->ny = 0;
    }
    else if (direction == 's')
    {
-      P->ny = -1;
-      P->nx = 0;
+      P->nx = 1;
+      P->ny = 0;
    }
    else if (direction == 'q')
    {
-      P->nx = -1;
-      P->ny = 0;  
+      P->ny = -1;
+      P->nx = 0;  
    }
    else if (direction == 'd')
    {
-      P->nx = 1;
-      P->ny = 0;
+      P->ny = 1;
+      P->nx = 0;
    }
    for  (int k=0;k<NR_GHOSTS;k++)
    {
@@ -211,5 +252,18 @@ int main()
             }
          }
       }
-   print_area(area);
+   area[depart_PacMan.x][depart_PacMan.y]='P';
+   initialize(allGhosts,area);
+   while (Pm.lives>=0)
+   {
+      if (Pm.lives>=0)
+      {
+         printf("Life : %d\nFood : %d\n",Pm.lives,Pm.food);
+      }
+      print_area(area);
+      clavier(&Pm, allGhosts);
+      move_Ghosts(allGhosts,area);
+      move_Pac_Man(&Pm, area);
+      check_life(&Pm);
+   }
 }
